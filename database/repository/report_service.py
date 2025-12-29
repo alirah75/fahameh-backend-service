d0a72adbc58edf8cb4db1c6c60937932450d61e5
Ali <alihey750@gmail.com>
2025-12-29 17:01:37 +0330

    add delete api for reports

--- database/repository/report_service.py
+++ database/repository/report_service.py
@@ -32,3 +32,25 @@ def commit_report_update(db: Session, report: Reports) -> Reports:
             detail=f"Failed to update report: {str(e)}"
         )
     return report
+
+
+def get_report_by_report_number(db: Session, report_No: str) -> Reports:
+    report = db.query(Reports).filter(Reports.Report_No == report_No).first()
+    if not report:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f'Report with RFI "{report_No}" not found.'
+        )
+    return report
+
+
+def delete_report_commit(db: Session, report: Reports):
+    try:
+        db.delete(report)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Failed to delete report: {str(e)}"
+        )
--- routers/route_reports.py
+++ routers/route_reports.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 from fastapi import Depends, APIRouter, HTTPException, status
 from database.repository.create_new_report import insert_new_report
-from database.repository.report_service import update_report_fields, commit_report_update, get_report_by_rfi
+from database.repository.report_service import update_report_fields, commit_report_update, get_report_by_rfi, \
+    delete_report_commit, get_report_by_report_number
 from database.session import get_db
 
 from database.repository.get_one_report import find_report
@@ -86,3 +87,14 @@ def update_report(
     report = update_report_fields(report, data)
     report = commit_report_update(db, report)
     return {"message": "Report updated successfully", "data": report}
+
+
+@router.delete("/report/", status_code=200)
+def delete_report(
+    report_no: str,
+    current_user: str = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    report = get_report_by_report_number(db, report_no)
+    delete_report_commit(db, report)
+    return {"message": "Report deleted successfully"}
